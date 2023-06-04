627a69f140f6abe1a511e0d30887be62f000eed0
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-06-04 19:42:38 -0300

    refactor sidebar avatar source

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
 
       <div className={styles.profile}>
-        <Avatar src={"https://github.com/maykbrito.png"}/>
+        <Avatar src={"https://github.com/felipprodrigues.png"}/>
 
         <strong>Nome</strong>
         <span>Web Dev</span>
